a0fe2678e4731deed6560b28051ffcac0f4832bb
ajagadeesh45 <ajagadeesh695@gmail.com>
2025-09-13 14:29:43 +0530

    add

---
+++ projectcar/car/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . models import Car
+# Register your models here.
+
+admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('car_id','brand','model','year','price')
+    
